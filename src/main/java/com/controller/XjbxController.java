b6a355c3f6b60f4027251b0860a07ba452f12cf4
hs <il>
2019-12-11 18:45:47 +0800

    发发发

--- src/main/java/com/controller/XjbxController.java
+++ src/main/java/com/controller/XjbxController.java
@@ -25,7 +25,7 @@ public class XjbxController {
 	public void xx1(@RequestBody(required=false) Map<String,Object> map) {
 //		Object object = map.get("pageNo");
 //		System.out.println(object+"////////////////");
-		System.out.println("???<><fff<><><><");
+		System.out.println("???<><fffddd><");
 	}
 	@PostMapping("/xx2")
 	public List<Emp> xx2(@RequestBody Emp emp) {
