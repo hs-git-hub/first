9f86a0862e8e3f71fdb99f209ddb68c134c9757a
hs-git-hub <58681559+hs-git-hub@users.noreply.github.com>
2019-12-11 18:19:51 +0800

    Update XjbxController.java

--- src/main/java/com/controller/XjbxController.java
+++ src/main/java/com/controller/XjbxController.java
@@ -26,6 +26,7 @@ public class XjbxController {
 //		Object object = map.get("pageNo");
 //		System.out.println(object+"////////////////");
 		System.out.println("???<><><<><><><><><");
+                System.out.println("???<><><<><><><><><");
 	}
 	@PostMapping("/xx2")
 	public List<Emp> xx2(@RequestBody Emp emp) {
